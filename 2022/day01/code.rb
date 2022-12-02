b345bc1bd8cdb3181eb260b1f569eb020fa24df7
Adam Cooke <adam@adam.ac>
2022-12-02 07:59:00 +0000

    move day01 into 2022

--- 2022/day01/code.rb
+++ 2022/day01/code.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-data = File.read("list.txt")
+data = File.read('input.txt')
 elfs = data.split("\n\n").map { |s| s.split("\n").map(&:to_i).sum }.sort
 puts "Top elf: #{elfs.last}"
 puts "Top 3 elfs: #{elfs.last(3).sum}"
